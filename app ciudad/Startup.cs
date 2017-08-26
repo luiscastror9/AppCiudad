3919f71aca6ae71f96467e684793c83a28e5eae4
Matias <gnu.matias@gmail.com>
2017-08-25 21:29:40 -0300

    version1

---
+++ app ciudad/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(app_ciudad.Startup))]
+namespace app_ciudad
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
