d31774bb9b1f1f32e0ba753ae27a765a8c7adbad
Matias <gnu.matias@gmail.com>
2017-09-22 18:42:28 -0300

    actualizacion botonera
    botonera

--- Lamarque_ciudad/Lamarque_ciudad/Controllers/usersController.cs
+++ Lamarque_ciudad/Lamarque_ciudad/Controllers/usersController.cs
@@ -11,43 +11,44 @@ using Lamarque_ciudad;
 
 namespace Lamarque_ciudad.Controllers
 {
-    public class peopleController : Controller
+    public class usersController : Controller
     {
         private DB_A2A1B8_netbd1Entities db = new DB_A2A1B8_netbd1Entities();
 
-        // GET: people
+        // GET: users
         public async Task<ActionResult> Index()
         {
             return View(await db.people_bd.ToListAsync());
         }
 
-        // GET: people/Details/5
-        public async Task<ActionResult> Details(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            people_bd people_bd = await db.people_bd.FindAsync(id);
-            if (people_bd == null)
-            {
-                return HttpNotFound();
-            }
-            return View(people_bd);
-        }
+     
 
-        // GET: people/Create
+       // GET: users/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: people/Create
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
-        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+    // GET: users/Login
+    public ActionResult Login()
+    {
+        return View();
+    }
+    [HttpPost]
+    public ActionResult Login(FormCollection collection )
+    {
+       
+         HttpContext.Session.Add("logged", true);
+
+        return View("~/Views/Home/Index.cshtml");
+    }
+
+    // POST: users/Create
+    // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+    // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "user,photo,pass,name,lastname,birth,mail")] people_bd people_bd)
+        public async Task<ActionResult> Create([Bind(Include = "user,pass,name,lastname,birth,mail,id")] people_bd people_bd)
         {
             if (ModelState.IsValid)
             {
@@ -59,8 +60,8 @@ namespace Lamarque_ciudad.Controllers
             return View(people_bd);
         }
 
-        // GET: people/Edit/5
-        public async Task<ActionResult> Edit(string id)
+        // GET: users/Edit/5
+        public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
             {
@@ -74,12 +75,12 @@ namespace Lamarque_ciudad.Controllers
             return View(people_bd);
         }
 
-        // POST: people/Edit/5
+        // POST: users/Edit/5
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
         // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "user,photo,pass,name,lastname,birth,mail")] people_bd people_bd)
+        public async Task<ActionResult> Edit([Bind(Include = "user,pass,name,lastname,birth,mail,id")] people_bd people_bd)
         {
             if (ModelState.IsValid)
             {
@@ -90,8 +91,8 @@ namespace Lamarque_ciudad.Controllers
             return View(people_bd);
         }
 
-        // GET: people/Delete/5
-        public async Task<ActionResult> Delete(string id)
+        // GET: users/Delete/5
+        public async Task<ActionResult> Delete(int? id)
         {
             if (id == null)
             {
@@ -105,24 +106,16 @@ namespace Lamarque_ciudad.Controllers
             return View(people_bd);
         }
 
-        // POST: people/Delete/5
+        // POST: users/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(string id)
+        public async Task<ActionResult> DeleteConfirmed(int id)
         {
             people_bd people_bd = await db.people_bd.FindAsync(id);
             db.people_bd.Remove(people_bd);
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+    
     }
 }
