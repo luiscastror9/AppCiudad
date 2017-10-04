268321bd5c26fe6a9dcbd68ec1a5d3c9ec12b11f
Matias <gnu.matias@gmail.com>
2017-10-04 19:12:15 -0300

    Edit2
    Buscador

--- Lamarque_web/Lamarque_web/Controllers/HomeController.cs
+++ Lamarque_web/Lamarque_web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lamarque_web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace Lamarque_web.Controllers
 {
     public class HomeController : Controller
     {
+        private DB_A2A1B8_netbd1Entities db = new DB_A2A1B8_netbd1Entities();
+
         public ActionResult Index()
         {
             return View();
@@ -23,6 +26,19 @@ namespace Lamarque_web.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Buscar(FormCollection collection)
+        {
+            string val = collection["busqueda_txt"];
+            List<eventos_bd> x = db.eventos_bd.Where(a => a.descripcion.Contains(val)).ToList();
+            List<servicios_bd> y = db.servicios_bd.Where(a => a.descripcion.Contains(val) || a.tipo.Contains(val) || a.nombre.Contains(val)).ToList();
+            Models.resultadobusqueda res = new Models.resultadobusqueda();
+
+            res.eventos = x;
+            res.servicios = y;
+            return View(res);
         }
     }
 }
